1aa28c3c7d8654f9717d8773c01ed68a16acb0dd
csteinmetz1 <csteinmetz1@gmail.com>
2018-04-01 23:04:39 -0400

    finalizng data structure - saving spectrograms to pickle file

--- pre_process.py
+++ pre_process.py
@@ -2,6 +2,7 @@ import glob
 import sys
 import os
 from collections import OrderedDict
+import pickle
 import scipy.io.wavfile as wavfile
 import numpy as np
 import pandas as pd
@@ -47,33 +48,47 @@ def level_analysis():
     # create dataframe and save result to csv
     dataframe = pd.DataFrame(database)
     dataframe.to_csv("level_analysis.csv", sep=',')
-    print("Saved level data from {0} tracks".format(len(database)))
+    print("Saved level data for {0} tracks".format(len(database)))
 
-def spectral_analysis(song_dir, save_data=True, save_img=False):
-    if not os.path.isdir(os.path.join(song_dir, "img")):
-        os.makedirs(os.path.join(song_dir, "img"))
-    if not os.path.isdir(os.path.join(song_dir, "data")):
-        os.makedirs(os.path.join(song_dir, "data"))
-    for stem in glob.glob(os.path.join(song_dir, "normalized", "*.wav")):
-        track_id = stem.split('/')[len(stem.split('/'))-3][0:3]
-        stem_class = stem.split('/')[len(stem.split('/'))-1].split('.')[0]
-        y, sr = librosa.load(stem, sr=22050, mono=True)
-        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+def spectral_analysis(save_data=True, save_img=False):
+    # dict to store columns
+    database = []
+    for idx, song in enumerate(glob.glob("DSD100/Sources/**/*")):
+        track_id = song.split('/')[len(song.split('/'))-1][0:3]
+        track_type = song.split('/')[len(song.split('/'))-2]
 
-        if save_data:
-            np.savetxt(os.path.join(song_dir, "data", stem_class + ".txt"), S, delimiter=',')
+        if not os.path.isdir(os.path.join(song, "img")):
+            os.makedirs(os.path.join(song, "img"))
+
+        database.append(OrderedDict({'type' : track_type,
+                                    'track id' : track_id, 
+                                    'bass spect' : [],
+                                    'drums spect' : [],
+                                    'other spect' : [],
+                                    'vocals spect' : []}))
+
+        for stem in glob.glob(os.path.join(song, "normalized", "*.wav")):
+            stem_class = stem.split('/')[len(stem.split('/'))-1].split('.')[0]
+            y, sr = librosa.load(stem, sr=44100, mono=True, duration=30.0)
+            y_22k = librosa.resample(y, sr, 22050)
+            S = librosa.feature.melspectrogram(y=y_22k, sr=22050, n_fft=2048, hop_length=1024, n_mels=128)
+            database[idx][stem_class + ' spect'] = S
+
+            if save_img:
+                plt.figure(figsize=(10, 4))
+                librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', x_axis='time')
+                plt.colorbar(format='%+2.0f dB')
+                plt.title('Mel spectrogram of normalized ' + stem_class)
+                plt.tight_layout()
+                plt.savefig(os.path.join(song, "img", stem_class + ".png")) 
+
             sys.stdout.write("Saved Mel spectrogram data of track {1} - {0}   \r".format(stem_class, track_id))
             sys.stdout.flush()
 
-        if save_img:
-            plt.figure(figsize=(10, 4))
-            librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', x_axis='time')
-            plt.colorbar(format='%+2.0f dB')
-            plt.title('Mel spectrogram of normalized ' + stem_class)
-            plt.tight_layout()
-            plt.savefig(os.path.join(song_dir, "img", stem_class + ".png")) 
+        sys.stdout.write("Spectral analysis complete for track {0}             \n".format(track_id)) 
 
-    sys.stdout.write("Spectral analysis complete for track {0}             \n".format(track_id)) 
+    # save ordereddict to pickle
+    pickle.dump(database, open("spectral_analysis.pkl", "wb"))
+    print("Saved spectral data for {0} tracks".format(len(database)))
 
-spectral_analysis("/Users/christiansteinmetz/Documents/MLGAN/DSD100/Sources/Test/049 - Young Griffo - Facade")
-spectral_analysis("/Users/christiansteinmetz/Documents/MLGAN/DSD100/Sources/Test/005 - Angela Thomas Wade - Milk Cow Blues")
+spectral_analysis()
