f51b5fe71e2e70a0cbe645b064fbd46b5b218154
csteinmetz1 <csteinmetz1@gmail.com>
2018-04-05 19:10:39 -0400

    modularizing

--- train_cnn.py
+++ train_cnn.py
@@ -16,82 +16,93 @@ import pickle
 batch_size = 2
 epochs = 100
 
-# stack subgroup spectrograms as channels
-dstack = True 
-# import mel spectrogram data
-spectral_analysis = pickle.load(open("spectral_analysis.pkl", "rb"))
+def load_data():
+    # import mel spectrogram data
+    spectral_analysis = pickle.load(open("spectral_analysis.pkl", "rb"))
 
-# sort out train and test data
-x_train_rows = [row for row in spectral_analysis if row['type'] == 'Dev']
-x_test_rows = [row for row in spectral_analysis if row['type'] == 'Test']
+    # sort out train and test data
+    x_train_rows = [row for row in spectral_analysis if row['type'] == 'Dev']
+    x_test_rows = [row for row in spectral_analysis if row['type'] == 'Test']
 
-# stack spectrograms side by side
-if dstack:
-    x_train = np.array([np.dstack((row['bass spect'], row['drums spect'], row['other spect'], row['vocals spect'])) for row in x_train_rows])
-    x_test = np.array([np.dstack((row['bass spect'], row['drums spect'], row['other spect'], row['vocals spect'])) for row in x_test_rows])
-else:
-    x_train = np.array([np.hstack((row['bass spect'], row['drums spect'], row['other spect'], row['vocals spect'])) for row in x_train_rows])
-    x_test = np.array([np.hstack((row['bass spect'], row['drums spect'], row['other spect'], row['vocals spect'])) for row in x_test_rows])
-    x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
-    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
-    
-spect_rows, spect_cols = x_train.shape[1], x_train.shape[2]
-input_shape = (spect_rows, spect_cols, 4)
+    # stack subgroup spectrograms as channels
+    dstack = True 
 
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+    if dstack:
+        x_train = np.array([np.dstack((row['bass spect'], row['drums spect'], row['other spect'], row['vocals spect'])) for row in x_train_rows])
+        x_test = np.array([np.dstack((row['bass spect'], row['drums spect'], row['other spect'], row['vocals spect'])) for row in x_test_rows])
+        input_shape = (x_train.shape[1], x_train.shape[2], 4)
+    else:
+        x_train = np.array([np.hstack((row['bass spect'], row['drums spect'], row['other spect'], row['vocals spect'])) for row in x_train_rows])
+        x_test = np.array([np.hstack((row['bass spect'], row['drums spect'], row['other spect'], row['vocals spect'])) for row in x_test_rows])
+        x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+        x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+        input_shape = (x_train.shape[1], x_train.shape[2], 1)
 
-# import output (level anaylsis) data
-level_analysis = pd.read_csv("level_analysis.csv")
+    print('x_train shape:', x_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
 
-# sort out train and test data
-y_train_rows = level_analysis.loc[level_analysis['type'] == 'Dev']
-y_test_rows = level_analysis.loc[level_analysis['type'] == 'Test']
+    # import output (level anaylsis) data
+    level_analysis = pd.read_csv("level_analysis.csv")
 
-# crate array of length 4 arrays
-y_train = np.array([np.array([row[1]['bass ratio'], row[1]['drums ratio'], row[1]['other ratio'], row[1]['vocals ratio']]) for row in y_train_rows.iterrows()])
-y_test = np.array([np.array([row[1]['bass ratio'], row[1]['drums ratio'], row[1]['other ratio'], row[1]['vocals ratio']]) for row in y_test_rows.iterrows()])
+    # sort out train and test data
+    y_train_rows = level_analysis.loc[level_analysis['type'] == 'Dev']
+    y_test_rows = level_analysis.loc[level_analysis['type'] == 'Test']
 
-# build network
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(4, 4)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(4))
+    # crate array of length 4 arrays
+    y_train = np.array([np.array([row[1]['bass ratio'], row[1]['drums ratio'], row[1]['other ratio'], row[1]['vocals ratio']]) for row in y_train_rows.iterrows()])
+    y_test = np.array([np.array([row[1]['bass ratio'], row[1]['drums ratio'], row[1]['other ratio'], row[1]['vocals ratio']]) for row in y_test_rows.iterrows()])
 
-model.compile(loss=losses.mean_squared_error, optimizer=optimizers.Adadelta(), metrics=['accuracy'])
-model.summary()
+    # create lists of total dataset
+    X = np.concatenate((x_train, x_test))
+    Y = np.concatenate((y_train, y_test))
 
-# checkpoint to save weights
-filepath='checkpoints/checkpoint-{epoch:02d}-{loss:.4f}.hdf5'
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    return X, Y, input_shape
 
-# create lists of total dataset
-X = np.concatenate((x_train, x_test))
-Y = np.concatenate((y_train, y_test))
+def build_model(input_shape):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                    activation='relu',
+                    input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(4, 4)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(4))
 
-# K fold x-validation with 10 folds
-kf = KFold(n_splits=10)
-kf.get_n_splits(X)
+    model.compile(loss=losses.mean_squared_error, optimizer=optimizers.Adadelta(), metrics=['accuracy'])
+    model.summary()
 
-for train_index, test_index in kf.split(X):
-    print("Train:", train_index, "Test:", test_index)
-    x_train, x_test = X[train_index], X[test_index]
-    y_train, y_test = Y[train_index], Y[test_index]
-    model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              verbose=1,
-              validation_data=(x_test, y_test),
-              callbacks=[checkpoint])
+    return model
 
-    score = model.evaluate(x_test, y_test, verbose=0)
+def train_and_eval_model(model, X_train, Y_train, X_test, Y_test):
+
+    # checkpoint to save weights
+    filepath='checkpoints/checkpoint-{epoch:02d}-{loss:.4f}.hdf5'
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+
+    model.fit(X_train, Y_train,
+                batch_size=batch_size,
+                epochs=epochs,
+                verbose=1,
+                validation_data=(X_test, Y_test),
+                callbacks=[checkpoint])
+
+    score = model.evaluate(X_test, Y_test, verbose=0)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
+
+if __name__ == "__main__":
+    n_folds = 2
+    X, Y, input_shape = load_data()
+    kf = KFold(n_splits=n_folds)
+
+    for i, (train_index, test_index) in enumerate(kf.split(X)):
+        print("Running fold", i+1, "of", n_folds)
+        model = None # clear the model
+        model = build_model(input_shape)
+        train_and_eval_model(model, X[train_index], Y[train_index], X[test_index], Y[test_index])
+
+    
 
 
