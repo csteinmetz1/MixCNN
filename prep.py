2ce53d57e5d767195dd82140f03c1f7360f52c4a
csteinmetz1 <csteinmetz1@gmail.com>
2018-06-23 10:06:34 -0500

    fixing file naming problem

--- prep.py
+++ prep.py
@@ -28,23 +28,19 @@ def process_tracks(remove_orig=True):
 		print("Mixing accompaniment to mono and downsampling...")
 
 		# convert vocal track to mono and downsample to 16 kHz
-		subprocess.call("sox vocals.wav -r 16k -e floating-point -b 32 vocal_m16k.wav remix 1,2", 
+		subprocess.call("sox vocals.wav -r 16k -e floating-point -b 32 vocals_m16k.wav remix 1,2", 
 			cwd=os.path.abspath(track), shell=True)
 		print("Mixing vocals to mono and downsampling...")
 
 		if remove_orig:
 			# remove accompaniment stems
-			for stem in ['bass.wav', 'drums.wav', 'other.wav']:
+			for stem in ['bass.wav', 'drums.wav', 'other.wav', 'vocals.wav']:
 				stempath = os.path.join(track, stem)
 				os.remove(stempath)
 				print("Removed {}.".format(stem))
 
 			# remove 44.1 kHz stereo accompaniment mix
 			os.remove(os.path.join(track, "accomp.wav"))
-
-			# remove 44.1 kHz stereo vocals
-			os.remove(os.path.join(track, "vocals.wav"))
-
 
 process_tracks(remove_orig=True)
 remove_mixtures()
