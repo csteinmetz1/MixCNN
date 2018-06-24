ba736af6c4a6115f6e5ce2fd4203ca2cabaf685f
csteinmetz1 <csteinmetz1@gmail.com>
2018-06-24 12:31:36 -0500

    model updates to sb model to train on modified version

--- models.py
+++ models.py
@@ -53,9 +53,9 @@ def build_ds_model(input_shape, rate, n_dft, n_hop, summary=False):
                         trainable_kernel=False,
                         name='mel'))
     model.add(SeparableConv1D(32, (8), activation='relu', input_shape=input_shape))
-    model.add(MaxPooling1D(pool_size=(4))
+    model.add(MaxPooling1D(pool_size=(4)))
     model.add(SeparableConv1D(32, (8), activation='relu'))
-    model.add(MaxPooling1D(pool_size=(4))
+    model.add(MaxPooling1D(pool_size=(4)))
     model.add(Dense(100))
     model.add(Dense(1))
     model.compile(loss=mean_squared_error, optimizer=Adam())
@@ -68,21 +68,22 @@ def build_ds_model(input_shape, rate, n_dft, n_hop, summary=False):
 def build_sb_model(input_shape, rate, n_dft, n_hop, summary=False):
     model = Sequential()
     model.add(Melspectrogram(n_dft=n_dft, n_hop=n_hop, input_shape=input_shape,
-                        padding='same', sr=rate, n_mels=96,
+                        padding='same', sr=rate, n_mels=128,
                         fmin=0.0, fmax=rate/2, power_melgram=1.0,
                         return_decibel_melgram=False, trainable_fb=False,
                         trainable_kernel=False,
                         name='mel'))
     model.add(SeparableConv2D(24, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
-    model.add(MaxPooling2D(pool_size=(2, 4)))
+    model.add(MaxPooling2D(pool_size=(4, 2)))
     model.add(SeparableConv2D(48, (5, 5), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 4)))
+    model.add(MaxPooling2D(pool_size=(4, 2)))
     model.add(SeparableConv2D(48, (5, 5), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 6)))
     model.add(Flatten())
-    model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
+    #model.add(Dropout(0.5))
+    model.add(Dense(1024, activation='relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(512, activation='relu'))
+    #model.add(Dropout(0.5))
     model.add(Dense(1, activation='linear'))
 
     model.compile(loss=mean_squared_error, optimizer=Adam())
--- train.py
+++ train.py
@@ -1,21 +1,26 @@
 import os
+import sys
 import numpy as np
 import models
 import util
 
-#models.build_cfm_model((2, 480000), 160000, summary=True)
-#models.build_sb_model((2, 480000), 160000, summary=True)
+#models.build_cfm_model((1, 349200), 160000, 256, 256, summary=True)
+#models.build_sb_model((2, 8.15*16000), 160000, n_dft=1024, n_hop=1024, summary=True)
 #models.test_model((128, 128, 1), 160000, summary=True)
+#sys.exit()
 
 # selected hyperparameters
 batch_size = 1
-epochs = 50
-n_dft = 512
-n_hop = 256
-model_type = 'cfm' 	# 'sb' or 'cfm'
+epochs = 400
+n_dft = 1024
+n_hop = 1024
+model_type = 'sb' 	# 'sb' or 'cfm'
 train = 'single' 	# 'single' or 'k-fold'
 
-X, Y = util.load(framing=False)
+# for cfm size use frame_length=21.825 - n_dft=256, n_hop=256
+# for sb size use frame_length=8.15 - n_dft=1024, n_hop=1024
+
+X, Y = util.load(frame_length=8.15, framing=True)
 
 if   train == 'k-fold':
 	for idx, val_track in enumerate(X):
@@ -40,12 +45,14 @@ elif train == 'single':
 	X_val   = X[0]
 	Y_val   = np.array([Y[0] for frame in X[0]])
 
+	del X, Y
+
 	print(X_train.shape)
 	print(Y_train.shape)
 	print(X_val.shape)
 	print(Y_val.shape)
 
-	model = models.build_cfm_model((X_train.shape[1], X_train.shape[2]), 
+	model = models.build_sb_model((X_train.shape[1], X_train.shape[2]), 
 										16000, n_dft, n_hop, summary=True)
 
 	history = model.fit(X_train, Y_train,
